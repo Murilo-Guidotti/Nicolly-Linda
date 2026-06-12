6b3c4ee0e50b956df0035d2b43385f6e78fc950d
v0agent <it+v0agent@vercel.com>
2026-06-12 23:25:19 +0000

    feat: update gallery photos and captions
    Updated photo sources and captions for Gallery component
    
    Co-authored-by: Murilo Guidotti <65193130+Murilo-Guidotti@users.noreply.github.com>

--- components/gallery.tsx
+++ components/gallery.tsx
@@ -19,9 +19,9 @@ export function Gallery() {
         <PhotoFrame rotate="-3deg" caption="o começo" src="/nicolly-onibus.jpg" alt="Nicolly na viagem" />
         <PhotoFrame rotate="2deg" caption="risadas" src="/nicolly-festa.jpg" alt="Nicolly sorrindo" className="md:mt-10" />
         <PhotoFrame rotate="-1deg" caption="A primeira vez na sua casa" src="/nicolly-cozinha.jpg" alt="Nicolly cozinhando" />
-        <PhotoFrame rotate="3deg" caption="aconchego" src="/nicolly-moletom.jpg" alt="Nicolly aconchegada" className="md:mt-8" />
-        <PhotoFrame rotate="-2deg" caption="você" src="/nicolly-cartas.jpg" alt="Nicolly jogando cartas" />
-        <PhotoFrame rotate="1deg" caption="a gente" src="/nicolly-casal-2.jpg" alt="A gente juntos" className="md:mt-6" />
+        <PhotoFrame rotate="3deg" caption="A Aliança" src="/nicolly-anel.jpg" alt="Nicolly aliança" className="md:mt-8" />
+        <PhotoFrame rotate="-2deg" caption="O seu lindo cabelo vermelho" src="/nicolly-cabelo.jpg" alt="Nicolly jogando cartas" />
+        <PhotoFrame rotate="1deg" caption="esse corpinho que eu amo" src="/nicolly-corpo.jpg" alt="A gente juntos" className="md:mt-6" />
       </div>
     </section>
   )
